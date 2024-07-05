d117902df1d9f29fde8966d914a069db221f1eee
CeAcheRistian <c.monroy.gtz@gmail.com>
2024-07-04 18:52:20 -0600

    add file

---
+++ web/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import request ##request es para poder consumir a traves de los pequeños clientes. se puede consumir  de 3 formas con args.get, args.form y json
+##Se puede enviar la info a traves de 3 formas, un formulario, un argunmento(lo que esta en un url) y un json
+app = Flask(__name__)#es el cliente principal
+@app.route("/") #es un decorador y con route se especifica la ruta del url
+def hello(): 
+    return "hello"
+
+@app.route("/hello")
+def hello2():
+    return "hi"
+
+@app.route("/suma/<int:num1>/<int:num2>/<int:num3>") #Se especifican las variables que se van a usar
+def suma(num1, num2,num3): #tambien en los parametros
+    num1 = request.args.get("num1", num1) #es una forma de consumir los parametros que se le mandan
+    num2 = request.args.get("num2", num2)
+    num3 = request.args.get("num3", num3)
+    return str(num1+num2+num3)
+
+if __name__ == "__main__":
+    app.run(debug=True,port=5002) #el run es para correr el servidor, es necesario para visualizar los cambios
+    #Es debug es para hacer cambios sin tener que matar el servidor, los cambios se hacen en vivo, port es para cambiar el puerto
+
+    #Existen 3 ramas de desallorro: QA es para pruebas, Prod o PDR es lo que se manda para consumo de lado del cliente, y Dev, que es el desarrollo
+    
---
+++ web/app2.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import request, render_template
+
+app = Flask(__name__)
+
+@app.route("/") 
+@app.route("/index")#tambien puede ser /index o /index/ #Entre esta linea y la funcion no puede haber espacios
+def index(): #a la raiz principal se le denomina index
+    return render_template("index.html") #se especifica el archivo html, el cual debe estar dentro de una carpeta llamada templates
+    #template es el archivo o plantilla para el rellenado de la vista
+
+
+@app.route("/hello")
+def hello(): 
+    return render_template("hello.html") 
+
+if __name__ == "__main__":
+    app.run(debug=True)

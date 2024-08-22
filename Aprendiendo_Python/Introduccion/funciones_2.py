a7bc7b4d27415341c9987e4c9f13551d68a253b5
CeAcheRistian <c.monroy.gtz@gmail.com>
2024-08-22 12:10:19 -0600

    add files

--- Aprendiendo_Python/Introduccion/funciones_2.py
+++ Aprendiendo_Python/Introduccion/funciones_2.py
@@ -3,9 +3,14 @@
 animal='Leon'
 def ejemplo():
     animal='Perro' #Se podría creer que el valor de animal, cambiaría para el resto del programa, pero no, solo cambia su valor, dentro del scope de la función
-    print(animal)
+    print(animal) #Aunque posean el mismo nombre de varible, para Python son diferentes
 ejemplo()
 print(animal) #La variable animal conseva su valor, ya que fue creada fuera de un bloque de código, denominandose el scope global
 
 #Las variables globales pueden ser usadas en todo el código, dentro de funciones, condicionales y ciclos.
 #Mientras que las variables locales, son eso, locales, solo pueden ser usadas dentro de su bloque de código donde fueron usadas
+
+#Es posible modificar el valor de una variable global, dentro de un bloque de código, con la palabra reservada global, seguido del nombre de la varible
+def ejemplo2():
+    global animal
+    animal = 'Gato'

fe3aa279b724f482350b4738dc7fd74f2a231a23
CeAcheRistian <c.monroy.gtz@gmail.com>
2024-09-03 11:01:32 -0600

    new readme

---
+++ comandos_github.py
@@ -0,0 +1,16 @@
+
+"""
+Con < git add archivo.py > se agregan archivos
+
+Con < git commit -m "Mensaje en ingles" > guardaS tus cambios en el repositorio locaL
+
+Y con < git push > se suben al repositorio web
+
+< git pull>  siempre hay que tirarlo para no generar inconsistencias antes de hacer add, commit y push
+
+Con < git clone URL del repo que se quiere clonar > se clona un repo en los archivos locales
+
+git status Para saber que se está añadiendo y actualizando
+
+git pull origin main --rebase Es para anterior mente haces un commit pero no lo has guardado en el repositorio
+"""

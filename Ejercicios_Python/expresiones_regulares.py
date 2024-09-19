1f12a55bf6c0c5f0bfa4cc117ca8f7d535444d09
CeAcheRistian <c.monroy.gtz@gmail.com>
2024-09-19 14:09:39 -0600

    add file

--- Ejercicios_Python/expresiones_regulares.py
+++ Ejercicios_Python/expresiones_regulares.py
@@ -3,15 +3,29 @@ de Python y disponible a través del módulo re. Usando este pequeño lenguaje, 
 coincidir; este conjunto puede contener frases en inglés, o direcciones de correo electrónico, o comandos TeX, o cualquier cosa que desee"""
 import re
 
+#https://cheatography.com/davechild/cheat-sheets/regular-expressions/
+#https://docs.python.org/es/3/library/re.html#module-re
+
+#Expresión regular para encontrar números en una cadena
 cadena = 'Hola,21 esta es 32 una 10000cadena de 901 texto7'
 pattern = r"\d+"
 match = re.findall(pattern, cadena)
 print(match)
 
-email = 'c.monroy@ciencias.unam.mx'
-pattern = r'\w+'
-match = re.findall(pattern, email)
+#Expresión regular para validar email
+email = 'c.monroy.gtz@gmail.com'
+pattern = r'^([\w._]+@\w+\.[a-z]{2,3})$'
+match = re.search(pattern, email)
 print(match)
 
-if re.fullmatch(pattern, email):
-    print('tu email es aceptable')
+#Expresión regular para validar url
+url = "https://www.google.com"
+pattern = r'^(https?://(www\.)?|www\.)(\w+)\.?(\w+)*\.[a-z]{2,3}$'
+match = re.search(pattern, url)
+print(match)
+
+#Expresión regular para validar número de teléfono
+telefono = '55-43-22-99-52'
+pattern = r'^(\d{2}\D?){5}$'
+match = re.search(pattern, telefono)
+print(match)

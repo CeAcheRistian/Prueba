27192b1bdf588e83412c5096c0d967d0b3671fd2
CeAcheRistian <c.monroy.gtz@gmail.com>
2024-09-24 18:10:04 -0600

    add files

--- Aprendiendo_Python/Inter/anotaciones.py
+++ Aprendiendo_Python/Inter/anotaciones.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Dict, Final, Union, Any, Optional, Literal, Self
+from typing import List, Tuple, Dict, Final, Union, Any, Optional, Literal, Self, NamedTuple, TypeVar
 
 #Es posible especificarle al lector que una variable, funcion, conjunto y clase no cambien de tipo de dato
 """Python ignora practicamente esta anotación, es más una ayuda para el lector que para el intérprete de Pyhton"""
@@ -30,14 +30,16 @@ CONSTANTE: Final = 3.14159
 
 #Para una variable que puede almacenar uno de varios tipos de datos usamos Union[], se importa
 usuario: Union[str,None]
+
 #Ejemplo de Union en una funcion
 def ejemplo(number: int) -> Union[None,int]:
     if number == 0: return None
     return number + 1
+
 #La importación y el uso de Union puede ser sustituido por el caracter pipe( | )
 variable: int | str = ''
 
-#Para no especificar un tipo de dato de una variable se usa Any importado de typing
+#Para no especificar un tipo de dato de una variable se usa Any importado de typing, puede ser cualquier tipo esa variable 
 variable: Any
 
 #Existen Optional para las variables que pueden ser opcionales. Viene bien especificarlo, pero solo aplica en parámetros que son por default
@@ -76,3 +78,28 @@ def create_user(username: str,  email: str) -> User: #Especificamos que retorna 
 #Se especificia que esta variable es del tipo de la clase
 chris: User = create_user('Christian', 'prueba@correo.com')
 
+#Con NameTuple podremos crear clases inmutables. Todos los objetos instanciados serán inmutables
+class DataBaseSetting(NamedTuple):
+    #No se definen métodos solo atributos
+    username: str
+    password: str | None #Es posible aplicar todo lo anterior
+    port: int
+    database: str
+    
+database_config_1 =  DataBaseSetting('root', 'password123',3306,'Base_de_Prueba')
+print(database_config_1.username)
+
+
+#Se puede especificar que una variable es de tipo genérico, desconocida o puede variar su tipo. Dotando de flexibilidad en los tipos de datos
+#Son objetos/variables que no se sabe de que tipo sean
+#Con la T se especifíca un tipo genérico
+T = TypeVar('T') #el argumento es el "nombre"
+
+def first_element(collection: List[T]) -> T | None:
+    'Retorna el primer elemento de una colección'
+    if len(collection) == 0:
+        return None
+    return collection[0]
+
+#Se puede limitar que tan genérico se es:
+Number = TypeVar('number', int, float) #Aca solo se pueden numeros

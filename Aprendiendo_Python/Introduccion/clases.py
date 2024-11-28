b71361517a701720f45bad7ed4ba5d665706d730
CeAcheRistian <c.monroy.gtz@gmail.com>
2024-11-28 15:57:12 -0600

    add properties examples to clases.py

--- Aprendiendo_Python/Introduccion/clases.py
+++ Aprendiendo_Python/Introduccion/clases.py
@@ -98,3 +98,56 @@ class Circulo:
         return cls.pi * (radio ** 2)
 
 resultado = Circulo.area(2) #Ejecutamos los métodos de clase
+
+# Las properties son la forma pythonica de evitar la creación de métodos para obtener y modificar atributos de una clase. Esta función nos ayuda a convertir atributos de una clase en properties o managed attributes.
+# Hay dos manera de implementarlo, la primera y mejor, con decoradores
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+
+    @property
+    def radius(self):
+        """Radius property."""
+        print("Get radius")
+        return self._radius
+    
+    @radius.setter
+    def radius(self, value):
+        print("Set radius")
+
+    @radius.deleter
+    def radius(self):
+        print("Delete radius")
+        del self._radius
+
+"Con las properties podemos acceder a los atributos de la clase sin necesidad de crear mil métodos para cada uno de los atributos."
+
+# La segunda manera de hacer esto es así con un objeto que sea instancia de la función property y dentro mandamos a llamar a las funciones de la clase:
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+
+    def _get_radius(self):
+        print("Get radius")
+        return self._radius
+
+    def _set_radius(self, value):
+        print("Set radius")
+        self._radius = value
+
+    def _del_radius(self):
+        print("Delete radius")
+        del self._radius
+
+    radius = property(
+        fget=_get_radius,
+        fset=_set_radius,
+        fdel=_del_radius,
+        doc="The radius property."
+    )
+
+"""Para ambos casos: para acceder a estos solo intanciamos la clase y llamamos"""
+circle = Circle(10)
+circle.radius
+circle.radius = 50
+del circle.radius
